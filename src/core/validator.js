a9d39e4a41844820d23ce3a8ab7e33a3d0a15ccc
Egor <evn88fx64@gmail.com>
2020-09-30 22:04:11 +0400

    add build

--- src/core/validator.js
+++ src/core/validator.js
@@ -1,52 +1,43 @@
 /* eslint-disable no-useless-escape */
-/* eslint-disable max-len */
-export function validate(element, options, form) {
-  /**
-  ** string, number, phone, email
-   */
-  const el = element.value
-  const errorForm = document.getElementById(form)
-  const errorList = document.getElementById('errorList')
-  const errorMessages = []
-  let rgxTemplate = new RegExp()
+export function validate(elements = [], conditions = []) {
+  const isError = false
 
-  if (!el) {
-    errorMessages.push('Необходимо заполнить поле')
-    return false
-  }
-
-  if (Array.isArray(options)) {
-    options.forEach(o => {
-      if (o === 'phone') {
-        rgxTemplate = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
-        errorMessages.push('Неверный формат номера телефона')
-      }
-      if (o === 'string') {
-        rgxTemplate = /^[a-zA-Zа-яА-Я]/;
-        errorMessages.push('Допустим ввод только текста')
-      }
-      if (o === 'email') {
-        rgxTemplate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        errorMessages.push('Неверный формат e-mail')
-      }
-    })
-
-    if (rgxTemplate.test(String(el))) {
-      errorForm.style.display = 'none'
-      return true
-    } else {
-      errorForm.style.display = 'block'
-
-      const li = document.createElement('li')
-
-      console.log(errorList.children)
-      errorMessages.forEach(error => {
-        li.innerText = error
-        errorList.append(li)
-      })
-      return false
+  const err = {
+    'phone': {
+      'msg': 'Неверный формат номера телефона',
+      'pattern': /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    },
+    'name': {
+      'msg': 'Введите Ваше реальное имя',
+      'pattern': /^[a-zA-Zа-яА-Я]/
+    },
+    'required': {
+      'msg': 'Поле не может быть пустым',
+      'pattern': /^\s*$/
     }
-  } else {
-    console.error('options is not an Array')
   }
+
+  for (let i = 0; i < elements.length; i++) {
+    const el = document.getElementById(elements[i])
+    const errorForm = el.parentElement.getElementsByClassName('error')
+    const input = el.value
+
+    errorForm[0].style.display = 'block'
+    errorForm[0].innerText = err.required.msg
+    console.log(input)
+    return isError
+  }
+
+  for (let i = 0; i < conditions.length; i++) {
+    // console.log(conditions)
+  }
+
+  // function delErrors() {
+  //   const errorItems = errorList.getElementsByTagName('li');
+  //   if (errorItems.length > 0) {
+  //     for (let i = 0; i < errorItems.length; i++) {
+  //       errorItems[i].remove()
+  //     }
+  //   }
+  // }
 }
--- src/index.js
+++ src/index.js
@@ -13,44 +13,46 @@ const list = document.querySelector('.list')
 
 
 form.addEventListener('submit', function(e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  const formName = document.getElementById('formName')
-  const formPhone = document.getElementById('formPhone')
+  // const formName = document.getElementById('formName')
+  // const formPhone = document.getElementById('formPhone')
 
+  /**
+   * 1) forms array
+   * 2) conditions array
+   */
   const formNameErr = validate(
-      formName,
-      ['required', 'string', 'min:3'],
-      'errorMessage'
+      ['formName', 'test'],
+      ['required', 'string', 'min:3']
   )
   const formPhoneErr = validate(
-      formPhone,
-      ['required', 'phone'],
-      'errorMessage'
+      ['formPhone'],
+      ['required', 'phone']
   )
 
   const listHTML =
    `<div class="list__item">
-      <div class="item item__name" contenteditable>${formName.value}</div>
-      <div class="item item__phone" contenteditable>${formPhone.value}</div>
+      <div class="item item__name form-control" contenteditable></div>
+      <div class="item item__phone form-control" contenteditable></div>
       <div class="item item__edit">
-        <a href="#"><span class="material-icons save">save_alt</span></a>
+        <a href="#"><span class="material-icons save">save</span></a>
         <a href="#"><span class="material-icons delete">delete</span></a>
       </div>
     </div>`;
 
-
-  if (formNameErr || formPhoneErr) {
-    // console.log('form: ', formName, formPhone)
-    formName.value = formPhone.value = null // clean form
-    formName.focus();
+  console.log('err: ', formNameErr, formPhoneErr)
+  if (formNameErr && formPhoneErr) {
+    list.insertAdjacentHTML('beforeEnd', listHTML)
   }
+  // formName.value = formPhone.value = null // clean form
+  // formName.focus()
 })
 
 // Обработка удаления
 
 list.addEventListener('click', function(e) {
   if (e.target.classList.contains('delete')) {
-    e.target.closest('.list__item').remove();
+    e.target.closest('.list__item').remove()
   }
 })
